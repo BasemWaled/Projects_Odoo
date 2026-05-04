d8ad8f5af83c79c5df9c27769fb31f52bed9a820
BASEM WALED <86613419+BasemWaled@users.noreply.github.com>
2026-05-04 20:57:54 +0300

    Delete app_one directory

--- app_one/models/models.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class PropertyModel(models.Model):
-    _name = "property.model"
-    _description = "Property Model"
-
-    name = fields.Char(string="Name")
-    postcode = fields.Char(string="Postcode")
-    date_available = fields.Date(string="Date Available")
